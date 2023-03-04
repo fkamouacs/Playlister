1fcf91ec9a940de4a05e08fe8c37ffe1ad67d3c3
Ferid Kamoua <kamouaferid@gmail.com>
2023-03-03 20:50:01 -0800

    added thumbnails to home playlist page

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -7,6 +7,7 @@ import FullPlaylist from "./fullPlaylist";
 const playlist = (props) => {
   const [currentSong, setCurrentSong] = useState();
   const [songs, setSongs] = useState([]);
+  const [thumbnail, setThumbnail] = useState();
   const router = useRouter();
   const { pathname } = router;
 
@@ -24,13 +25,34 @@ const playlist = (props) => {
     router.push(`/${props.username}`);
   };
 
+  useEffect(() => {
+    if (songs[0]) {
+      axios
+        .post("/api/playlists/getSong", { id: songs[0].song_id })
+        .then((res) => {
+          setThumbnail(getYoutubeId(res.data.link));
+        });
+    }
+  }, [songs]);
+
+  const getYoutubeId = (url) => {
+    var regExp =
+      /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    let id = match && match[7].length == 11 ? match[7] : false;
+    return `https://img.youtube.com/vi/${id}/0.jpg`;
+  };
+
   return (
     <div>
       {pathname == "/playlist/[id]" ? (
         <FullPlaylist songs={songs} data={props} />
       ) : (
         <Link className="flex flex-col m-1 " href={`/playlist/${props.id}`}>
-          <div className="h-[118px] w-[210px] bg-black rounded-lg"></div>
+          <img
+            className="h-[118px] w-[210px]  rounded-lg object-cover"
+            src={thumbnail}
+          />
           <h2 className="text-[14px] font-bold text-[#0F0F0F">{props.title}</h2>
           <div
             className="text-[12px] text-[#606060]"
